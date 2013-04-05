b1dd6a91fddbc701e0187d7036bd6e05d241c41b
Zumbi Azul <capoeira_basics@mail.ru>
2013-04-05 16:10:43 +0400

    Button added

--- src/com/flexymind/hw03app02/Flexymind_hw_03_app02Activity.java
+++ src/com/flexymind/hw03app02/Flexymind_hw_03_app02Activity.java
@@ -2,6 +2,9 @@ package com.flexymind.hw03app02;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class Flexymind_hw_03_app02Activity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,11 @@ public class Flexymind_hw_03_app02Activity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button btn = new Button(this);
+        LayoutParams btnParam = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        LinearLayout linLayout = (LinearLayout)findViewById(R.id.lin);
+        btn.setText("Button");
+        linLayout.addView(btn, btnParam);
     }
 }
