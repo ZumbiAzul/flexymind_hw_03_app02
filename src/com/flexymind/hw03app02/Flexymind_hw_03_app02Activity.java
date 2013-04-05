29ea4e475913c2a430d740bd7ba235147107b6f4
Zumbi Azul <capoeira_basics@mail.ru>
2013-04-06 00:07:01 +0400

    fixed Button click response that is random positioning. Edited AndroidManifest.xml

--- src/com/flexymind/hw03app02/Flexymind_hw_03_app02Activity.java
+++ src/com/flexymind/hw03app02/Flexymind_hw_03_app02Activity.java
@@ -1,5 +1,7 @@
 package com.flexymind.hw03app02;
 
+import java.util.Random;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.widget.LinearLayout;
 public class Flexymind_hw_03_app02Activity extends Activity implements OnClickListener{
 	
 	Button btn;
+	LinearLayout linLayout;
 	
     /** Called when the activity is first created. */
     @Override
@@ -18,9 +21,9 @@ public class Flexymind_hw_03_app02Activity extends Activity implements OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        Button btn = new Button(this);
+        btn = new Button(this);
         LayoutParams btnParam = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        LinearLayout linLayout = (LinearLayout)findViewById(R.id.lin);
+        linLayout = (LinearLayout)findViewById(R.id.lin);
         btn.setText("!!! Jump !!!");
         linLayout.addView(btn, btnParam);
         btn.setOnClickListener(this);
@@ -28,7 +31,14 @@ public class Flexymind_hw_03_app02Activity extends Activity implements OnClickLi
     
     public void onClick(View v) {
       // TODO Auto-generated method stub
-    	btn.setText("Jumped OK");
+    	int max1 = 100;
+    	int max2 = 200;
+    	
+    	Random r = new Random();
+    	
+    	int left = r.nextInt(max1);
+    	int top = r.nextInt(max2);
+    	linLayout.setPadding(left, top, 0, 0);
     }
     
 }
