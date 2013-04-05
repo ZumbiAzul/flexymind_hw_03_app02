d0895e2dda2fcae2fdd2218d5463b17209cafd31
Zumbi Azul <capoeira_basics@mail.ru>
2013-04-05 15:29:55 +0400

    Created a new Android Project using Eclipse

---
+++ src/com/flexymind/hw03app02/Flexymind_hw_03_app02Activity.java
@@ -0,0 +1,13 @@
+package com.flexymind.hw03app02;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Flexymind_hw_03_app02Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
