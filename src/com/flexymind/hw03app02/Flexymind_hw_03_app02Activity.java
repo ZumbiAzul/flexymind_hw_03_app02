4d2746a9de69a461d425b9388c4b05a5ace1e1f6
Zumbi Azul <capoeira_basics@mail.ru>
2013-04-05 16:28:23 +0400

    Text on Button changed to !!! JUMP !!!. Added onClick dummy. Now button just finishes the activity.

--- src/com/flexymind/hw03app02/Flexymind_hw_03_app02Activity.java
+++ src/com/flexymind/hw03app02/Flexymind_hw_03_app02Activity.java
@@ -2,11 +2,16 @@ package com.flexymind.hw03app02;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
-public class Flexymind_hw_03_app02Activity extends Activity {
+public class Flexymind_hw_03_app02Activity extends Activity implements OnClickListener{
+	
+	Button btn;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,7 +21,14 @@ public class Flexymind_hw_03_app02Activity extends Activity {
         Button btn = new Button(this);
         LayoutParams btnParam = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         LinearLayout linLayout = (LinearLayout)findViewById(R.id.lin);
-        btn.setText("Button");
+        btn.setText("!!! Jump !!!");
         linLayout.addView(btn, btnParam);
+        btn.setOnClickListener(this);
     }
+    
+    public void onClick(View v) {
+      // TODO Auto-generated method stub
+      finish();
+    }
+    
 }
